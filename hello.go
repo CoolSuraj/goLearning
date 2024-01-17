0a775a28b3e684a72223fdbb0d30c1072af94ed5
srjOfficial <suraj2office@duck.com>
2024-01-17 15:01:43 +0530

    modified hello.go

--- hello.go
+++ hello.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 import "rsc.io/quote"
 func main() {
+	//Printing Hello World
 	fmt.Println("Hello, World!")
 	fmt.Print(quote.Opt());
+
+	//Declaring variable
+	var n int = 300
+	fmt.Println(n)
 }
